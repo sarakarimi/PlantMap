b2519735f5d691e7304e83e8bf1c251697548d6e
Markus Fritzsche <fritzsma@gmail.com>
2024-11-18 09:15:44 +0100

    Update 'create_all_flowers' script

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # PlantMap
 PlantMap: federated learning for segmentation, classification, and detection of weed species in aerial images taken from farm fields for WASP course 2024.
+
+
+## Setup Environment
+
+```
+conda create --prefix <path to new conda env> python=3.12
+conda activate <path or name>
+conda install -c conda-forge huggingface_hub
+cd /tmp # or any empty directory
+git clone https://github.com/facebookresearch/sam2.git
+cd sam2
+pip install torch torchvision numpy
+pip install -e .
+pip install -e ".[notebooks]"
+pip install scipy
+
+```
+
+Place all images in data/Hyperlapse
+
+
+## Create Masks
+
+```
+mkdir masks
+mkdir test_results
+python huggingface_sam_test.py 
+
+```
+
+## Create Flower Dataset for Unsupervised Classification
+
+```
+conda activate <path or name>
+python create_all_flowers.py
+```
+
--- create_all_flowers.py
+++ create_all_flowers.py
@@ -13,37 +13,39 @@ def main() -> None:
     file_names = os.listdir(image_folder)
 
     flowers = []
-    with tqdm(file_names) as pbar:
-        for file_name in file_names:
-            if not file_name.endswith("JPG"):
-                continue
-            file_path = os.path.join(image_folder, file_name)
-            mask_file = file_name.replace("JPG", "pt")
-            mask_path = os.path.join(mask_folder, mask_file)
+    with torch.no_grad():
+        with tqdm(file_names) as pbar:
+            for file_name in pbar:
+                if not file_name.endswith("JPG"):
+                    continue
+                file_path = os.path.join(image_folder, file_name)
+                mask_file = file_name.replace("JPG", "pt")
+                mask_path = os.path.join(mask_folder, mask_file)
 
-            image_array = np.array(Image.open(file_path))
+                image_array = np.array(Image.open(file_path))
 
-            masks = torch.load(mask_path)
+                masks = torch.load(mask_path)
 
-            for i, mask in enumerate(masks):
-                bbox = mask["bbox"]
-                segmentation = mask["segmentation"].toarray()
-                size_x, size_y = segmentation.shape
+                for i, mask in enumerate(masks):
+                    bbox = mask["bbox"]
+                    segmentation = mask["segmentation"].toarray()
+                    size_x, size_y = segmentation.shape
 
-                x_min, y_min, width, height = bbox
-                x_min = int(x_min)
-                y_min = int(y_min)
-                width = int(width)
-                height = int(height)
+                    x_min, y_min, width, height = bbox
+                    x_min = int(x_min)
+                    y_min = int(y_min)
+                    width = int(width)
+                    height = int(height)
 
-                masked_image = image_array * segmentation.reshape(size_x, size_y, 1)
-                cropped_image = masked_image[
-                    y_min : y_min + height, x_min : x_min + width
-                ]
-                flowers.append(cropped_image)
+                    masked_image = image_array * segmentation.reshape(size_x, size_y, 1)
+                    cropped_image = masked_image[
+                        y_min : y_min + height, x_min : x_min + width
+                    ]
+                    flowers.append(cropped_image)
 
-                pbar.set_description_str(f"Progress: {i} / {len(masks)}")
-    torch.save(flowers, "data/flowers.pt")
+                    pbar.set_description_str(f"Progress: {i} / {len(masks)}")
+                torch.save(flowers, f"data/{file_name}.pt")
+                flowers = []
 
 
 if __name__ == "__main__":
