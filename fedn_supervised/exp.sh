0f9fbad4eb22eb2c4257bc036c1d0a949ce17c46
Markus Fritzsche <markus.fritzsche@i6.informatik.rwth-aachen.de>
2024-12-13 14:50:19 +0100

    Train on only 1 gpu

--- fedn_supervised/exp.sh
+++ fedn_supervised/exp.sh
@@ -5,9 +5,9 @@
 #SBATCH --nodes=1
 #SBATCH --time=0-05:00:00
 #SBATCH --mem=32G
-#SBATCH --account=Berzelius-2024-465
+#SBATCH --partition=rleap_gpu_24gb
 
 export NUM_AGENTS=6
 export AGENT_ID="$1"
 
-uv run --no-cache fedn client start -in client_files/client"$1".yaml --secure=True --force-ssl --local-package
+uv run --cache-dir uv_cache/ fedn client start -in client_files/client"$1".yaml --secure=True --force-ssl --local-package
