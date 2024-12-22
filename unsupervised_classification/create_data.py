e49c3ff1ea237cb55909552ef8a2d3b563396277
Markus Fritzsche <fritzsma@gmail.com>
2024-12-22 10:35:14 +0100

    Add script for masks data

---
+++ unsupervised_classification/create_data.py
@@ -0,0 +1,172 @@
+from cProfile import label
+
+import torch
+from sam2.sam2_image_predictor import SAM2ImagePredictor
+from sam2.automatic_mask_generator import SAM2AutomaticMaskGenerator
+from sam2.build_sam import build_sam2_hf
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from tqdm import tqdm
+from datasets import load_dataset
+
+
+# Define some helper functions
+def show_mask(mask, ax, random_color=False, borders=True):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
+    h, w = mask.shape[-2:]
+    mask = mask.astype(np.uint8)
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    if borders:
+        import cv2
+
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        # Try to smooth contours
+        contours = [
+            cv2.approxPolyDP(contour, epsilon=0.01, closed=True) for contour in contours
+        ]
+        mask_image = cv2.drawContours(
+            mask_image, contours, -1, (1, 1, 1, 0.5), thickness=2
+        )
+    ax.imshow(mask_image)
+
+
+def show_points(coords, labels, ax, marker_size=375):
+    pos_points = coords[labels == 1]
+    neg_points = coords[labels == 0]
+    ax.scatter(
+        pos_points[:, 0],
+        pos_points[:, 1],
+        color="green",
+        marker="*",
+        s=marker_size,
+        edgecolor="white",
+        linewidth=1.25,
+    )
+    ax.scatter(
+        neg_points[:, 0],
+        neg_points[:, 1],
+        color="red",
+        marker="*",
+        s=marker_size,
+        edgecolor="white",
+        linewidth=1.25,
+    )
+
+
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(
+        plt.Rectangle((x0, y0), w, h, edgecolor="green", facecolor=(0, 0, 0, 0), lw=2)
+    )
+
+
+def show_masks(
+    image,
+    masks,
+    scores,
+    point_coords=None,
+    box_coords=None,
+    input_labels=None,
+    borders=True,
+):
+    for i, (mask, score) in enumerate(zip(masks, scores)):
+        plt.figure(figsize=(10, 10))
+        plt.imshow(image)
+        show_mask(mask, plt.gca(), borders=borders)
+        if point_coords is not None:
+            assert input_labels is not None
+            show_points(point_coords, input_labels, plt.gca())
+        if box_coords is not None:
+            # boxes
+            show_box(box_coords, plt.gca())
+        if len(scores) > 1:
+            plt.title(f"Mask {i+1}, Score: {score:.3f}", fontsize=18)
+        plt.axis("off")
+        plt.show()
+
+
+def show_anns(anns, ax=None, borders=True):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x["area"]), reverse=True)
+
+    if ax == None:
+        ax = plt.gca()
+    ax.set_autoscale_on(False)
+
+    img = np.ones(
+        (
+            sorted_anns[0]["segmentation"].shape[0],
+            sorted_anns[0]["segmentation"].shape[1],
+            4,
+        )
+    )
+    img[:, :, 3] = 0
+    for ann in sorted_anns:
+        m = ann["segmentation"]
+        color_mask = np.concatenate([np.random.random(3), [0.5]])
+        img[m] = color_mask
+        if borders:
+            import cv2
+
+            contours, _ = cv2.findContours(
+                m.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
+            )
+            # Try to smooth contours
+            contours = [
+                cv2.approxPolyDP(contour, epsilon=0.01, closed=True)
+                for contour in contours
+            ]
+            cv2.drawContours(img, contours, -1, (0, 0, 1, 0.4), thickness=1)
+
+    ax.imshow(img)
+
+
+# Build the model
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+sam2 = build_sam2_hf(
+    "facebook/sam2.1-hiera-large", device=device, apply_postprocessing=False
+)
+
+# Load the model
+# predictor = SAM2ImagePredictor.from_pretrained("facebook/sam2.1-hiera-large")
+# mask_generator = SAM2AutomaticMaskGenerator(sam2)
+
+mask_generator_2 = SAM2AutomaticMaskGenerator(
+    model=sam2,
+    label="daisy",
+    points_per_side=32,
+    points_per_batch=64,
+    pred_iou_thresh=0.7,
+    stability_score_thresh=0.92,
+    stability_score_offset=0.7,
+    crop_n_layers=1,
+    box_nms_thresh=0.7,
+    crop_n_points_downscale_factor=2,
+    min_mask_region_area=25.0,
+    use_m2m=False,
+)
+
+
+ds = load_dataset("gotdairyya/plant_images")
+print("dataset loaded")
+
+# testing with one sample
+test_image = ds['train'][0]['image']
+images = [ds['train'][i]['image'] for i in range(len(ds["train"]))]
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+with torch.inference_mode(), torch.autocast(device, dtype=torch.bfloat16):
+    for test_image in tqdm(images, desc="Processing images"):
+        test_image = np.array(test_image.convert("RGB"))
+
+        masks = mask_generator_2.generate(test_image)
+        torch.save("../masks/mask.pt", masks)
